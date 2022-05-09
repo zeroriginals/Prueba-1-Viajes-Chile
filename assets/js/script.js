af0de5615afbb6a6e64285996fd24b4933f1b2d5
Javiera Iglesias <javierab.iglesias@gmail.com>
2022-05-09 13:19:11 +0200

    Version inicial

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*modal boton contacto*/
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
+
+
+
+
