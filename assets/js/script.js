e77194660c2766ee3690b16af8ea0726d28c07eb
Javiera Iglesias <javierab.iglesias@gmail.com>
2022-05-09 18:25:09 +0200

    Version dblclick en footer y smooth scroll navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-/* jquery  */
+
+//jquery y javascrip//
 
 $(document).ready(function () {
 
-    /*tooltip - iconos - quienes somos */
+    //tooltip - iconos - quienes somos //
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 
 
-    /* Toggle - img - card */
+    // Toggle - img - card //
     $(".card1").on("click", function () {
         $(".card-text1").toggle();
     });
@@ -23,7 +24,23 @@ $(document).ready(function () {
         $(".card-text4").toggle();
     });
 
+    //doble click - cambio color - footer
+    $(".texto-footer").on("dblclick", function () {
+        $(this).css({
+            "color": "blue",
+            "font-size": "1.5rem"
+        });
+    });
+
+    //smooth scroll//
+    $(".nav-link").on("click", function (e) {
+        e.preventDefault();
+        const href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+    });
+
 });
+
 
 
 
